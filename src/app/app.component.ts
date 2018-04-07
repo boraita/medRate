008d42f8f6fb9e51c66114433ee19a11858ced22
rmn <rmn@novatec-gmbh.de>
2018-04-07 20:55:31 +0200

    Added record sound and take value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { BeatAnalyzerService } from './beat-analyzer.service';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/bufferTime';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+  constructor(private beatAnalyzer: BeatAnalyzerService) {
+    this.beatAnalyzer.init();
+    this.beatAnalyzer.beat$
+      .filter(val => val > 0.05)
+      .bufferTime(100)
+      .filter(val => val.length > 0)
+      .subscribe();
+  }
+
+  get beat$() {
+    return this.beatAnalyzer.beat$;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-
+import { BeatAnalyzerService } from './beat-analyzer.service';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
+  providers: [BeatAnalyzerService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
