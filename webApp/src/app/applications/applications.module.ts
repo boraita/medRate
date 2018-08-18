8faf396e18edfe8543e97ec6ecf4c4febdad7ad2
rmn <rmn@novatec-gmbh.de>
2018-08-18 11:14:11 +0200

    Created chart rate component

--- webApp/src/app/applications/applications.module.ts
+++ webApp/src/app/applications/applications.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { ApplicationsRoutingModule } from './applications-routing.module';
 import { ApplicationsComponent } from './applications.component';
+import { ChartRateComponent } from './chart-rate/chart-rate.component';
 
 @NgModule({
   imports: [ApplicationsRoutingModule],
-  declarations: [ApplicationsComponent]
+  declarations: [ApplicationsComponent, ChartRateComponent]
 })
 export class ApplicationsModule {}
---
+++ webApp/src/app/applications/chart-rate/chart-rate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-rate',
+  templateUrl: './chart-rate.component.html',
+  styleUrls: ['./chart-rate.component.scss']
+})
+export class ChartRateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
